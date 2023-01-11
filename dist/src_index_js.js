917b530079b9fe5bd89b79f38b744ab004360c00
sainiteshGit <nitesh_4u@yahoo.com>
2023-01-10 22:24:09 -0500

    mfe1 part update

---
+++ dist/src_index_js.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkmicro_front_end_1"] = self["webpackChunkmicro_front_end_1"] || []).push([["src_index_js"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (function() {
+
+eval("document.querySelector('#root').innerHTML=`<h1>Micro-Front-End-1</h1>`\nconsole.log(\"micro-front-end-1\");\n\n//# sourceURL=webpack://micro-front-end-1/./src/index.js?");
+
+/***/ })
+
+}]);
