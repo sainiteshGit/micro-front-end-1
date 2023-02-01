6e85dc6543eb375fbf2eaa5ec9f77758697a0adf
sainiteshGit <nitesh_4u@yahoo.com>
2023-01-31 22:14:35 -0500

    mfe 1 update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-document.querySelector('#root').innerHTML=`<h1> Micro-Front-End-1 updates for the index....@@@@...!!!!!!!!!.....///./...</h1>`
+document.querySelector('#root').innerHTML=`<h1> Micro-Front-End-1 updates for the index....@@@@...!!!!!!!!!.....///./...////.....11!!!</h1>`
 console.log("micro-front-end-----1");
