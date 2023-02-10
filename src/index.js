967d89c20232d4c12e11692a4bb7aa293d53d37e
sainiteshGit <nitesh_4u@yahoo.com>
2023-02-10 18:09:04 -0500

    update the mfe 1code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-document.querySelector('#root').innerHTML=`<h1> Micro-Front-End-1 update ------for the index....@@@@.@@@@@@..!!!!!!!!!%%%%#####.........#######^^^^^^^#########..////...////.....11!!!</h1>`
+document.querySelector('#root').innerHTML=`<h1> Micro-Front-End-1 update ------for the index.!!!!!!!!...@@@@.@@@@@@..!!!!!!!!!%%%%#####.........#######^^^^^^^#########..////...////.....11!!!</h1>`
 console.log("micro-front-end-----1");
