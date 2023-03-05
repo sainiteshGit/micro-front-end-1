aaf9c1ef4f8a3cf3bd3a651ddb95b350150a2ed0
sainiteshGit <nitesh_4u@yahoo.com>
2023-03-04 23:55:57 -0500

    update the mfe1

--- dist/src_index_js.js
+++ dist/src_index_js.js
@@ -14,7 +14,7 @@
   \**********************/
 /***/ (function() {
 
-eval("document.querySelector('#root').innerHTML=`<h1> updates---ttt--ttt ###########-#######--######----update to the Micro-Front-End-1 update --@@@---#############-########for the index..%%%%..!!!.!!!!.!!!!!!!!...@@@@.@@@@@@..!!!!!!!!!%%%%#####.........#######^^^^^^^#########..////...////........##$$$$$11!!! $$$$$$</h1>`\nconsole.log(\"micro-front-end-----####1\");\n\n//# sourceURL=webpack://micro-front-end-1/./src/index.js?");
+eval("document.querySelector('#root').innerHTML=`<h1>###########-#######--######----update to the Micro-Front-End-1 update --@@@---#############-########for the index..%%%%..!!!.!!!!.!!!!!!!!...@@@@.@@@@@@..!!!!!!!!!%%%%#####.........#######^^^^^^^#########..////...////........##$$$$$11!!! $$$$$$</h1>`\nconsole.log(\"micro-front-end-----####1\");\n\n//# sourceURL=webpack://micro-front-end-1/./src/index.js?");
 
 /***/ })
 
